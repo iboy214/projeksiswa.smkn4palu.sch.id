4601ed77fe0f2d409b71d12640cacd2620e595c7
iboy214 <150139726+iboy214@users.noreply.github.com>
2023-12-19 02:16:01 +0800

    Create sv_berita_db.php

---
+++ sv_berita_db.php
@@ -0,0 +1,33 @@
+<?php
+// Koneksi ke database
+$dbHost = 'localhost';
+$dbUser = 'username'; // Ganti dengan username MySQL Anda
+$dbPass = 'password'; // Ganti dengan password MySQL Anda
+$dbName = 'news_db';
+
+$conn = new mysqli($dbHost, $dbUser, $dbPass, $dbName);
+
+// Cek koneksi
+if ($conn->connect_error) {
+    die("Koneksi ke database gagal: " . $conn->connect_error);
+}
+
+// Tangkap data dari formulir
+$title = $_POST['title'];
+$content = $_POST['content'];
+
+// Simpan gambar
+$imagePath = 'uploads/' . basename($_FILES['image']['name']);
+move_uploaded_file($_FILES['image']['tmp_name'], $imagePath);
+
+// Query SQL untuk menyimpan berita ke database
+$sql = "INSERT INTO news (title, content, image_path) VALUES ('$title', '$content', '$imagePath')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Berita berhasil disimpan!";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>

9ba5267d0e767cc6dd21e3c0d457e11e105dd55f
iboy214 <150139726+iboy214@users.noreply.github.com>
2023-12-19 02:33:49 +0800

    Update sv_berita_db.php

--- sv_berita_db.php
+++ sv_berita_db.php
@@ -1,30 +1,31 @@
 <?php
-// Koneksi ke database
-$dbHost = 'localhost';
-$dbUser = 'root'; // Ganti dengan username MySQL Anda
-$dbPass = ''; // Ganti dengan password MySQL Anda
-$dbName = 'berita_db';
+// Konfigurasi database
+$servername = "localhost";
+$username = "root"; // Ganti dengan username MySQL Anda
+$password = ""; // Ganti dengan password MySQL Anda
+$database = "beritaterkini"; // Ganti dengan nama database Anda
 
-$conn = new mysqli($dbHost, $dbUser, $dbPass, $dbName);
+// Membuat koneksi
+$conn = new mysqli($servername, $username, $password, $database);
 
-// Cek koneksi
+// Memeriksa koneksi
 if ($conn->connect_error) {
     die("Koneksi ke database gagal: " . $conn->connect_error);
 }
 
 // Tangkap data dari formulir
-$title = $_POST['title'];
-$content = $_POST['content'];
+$judul = $_POST['judul'];
+$isi = $_POST['isi'];
 
 // Simpan gambar
-$imagePath = 'uploads/' . basename($_FILES['image']['name']);
-move_uploaded_file($_FILES['image']['tmp_name'], $imagePath);
+$gambarPath = 'uploads/' . basename($_FILES['gambar']['name']);
+move_uploaded_file($_FILES['gambar']['tmp_name'], $gambarPath);
 
 // Query SQL untuk menyimpan berita ke database
-$sql = "INSERT INTO news (title, content, image_path) VALUES ('$title', '$content', '$imagePath')";
+$sql = "INSERT INTO berita (judul, isi, gambar_path) VALUES ('$judul', '$isi', '$gambarPath')";
 
 if ($conn->query($sql) === TRUE) {
-    echo "Berita berhasil disimpan!";
+    echo "Berita berhasil diunggah!";
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }

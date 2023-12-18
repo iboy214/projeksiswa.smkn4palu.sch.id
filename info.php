a45bc66b11a0eb14fcdc71e5343604146ae55822
iboy214 <150139726+iboy214@users.noreply.github.com>
2023-12-19 03:35:57 +0800

    Update info.php

--- info.php
+++ info.php
@@ -433,41 +433,6 @@ body{--wp--preset--color--black: #000000;--wp--preset--color--cyan-bluish-gray: 
     <h2>Berita Terbaru</h2>
     <ul id="news-items">
         <!-- Berita akan di tambahkan di bagian sini -->
-        
-<?php
-// Koneksi ke database
-$dbHost = 'https://iboy214.github.io/projeksiswa.smkn4palu.sch.id/info.html';
-$dbUser = 'root'; // Ganti dengan username MySQL Anda
-$dbPass = ''; // Ganti dengan password MySQL Anda
-$dbName = 'berita_db';
-
-$conn = new mysqli($dbHost, $dbUser, $dbPass, $dbName);
-
-// Cek koneksi
-if ($conn->connect_error) {
-    die("Koneksi ke database gagal: " . $conn->connect_error);
-}
-
-// Tangkap data dari formulir
-$title = $_POST['title'];
-$content = $_POST['content'];
-
-// Simpan gambar
-$imagePath = 'uploads/' . basename($_FILES['image']['name']);
-move_uploaded_file($_FILES['image']['tmp_name'], $imagePath);
-
-// Query SQL untuk menyimpan berita ke database
-$sql = "INSERT INTO news (title, content, image_path) VALUES ('$title', '$content', '$imagePath')";
-
-if ($conn->query($sql) === TRUE) {
-    echo "Berita berhasil disimpan!";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
-
-$conn->close();
-?>
-
     </ul>
 </section>
 
